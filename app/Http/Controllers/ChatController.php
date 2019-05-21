c85b58a0dd58c88cf0c3cea353e10e6dbaa5f5d1
PerhapsSomeone <ma2003br@gmail.com>
2019-05-21 15:06:25 +0200

    Added Chat controller

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -4,19 +4,46 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class ChatController extends Controller
 {
     //
     public function chatWindow(Request $request) {
-        if(!isset($request->input()["lehrer"]) || !is_int($request->input()["lehrer"]) || ($request->input()["lehrer"] < 0 || $request->input()["lehrer"] > 1)) {
+        if(!isset($request->input()["lehrer"]) || !is_bool((boolean)$request->input()["lehrer"])) {
             return redirect("home");
         }
 
-        if($request->input()["lehrer"] === Auth::user()["lehrer"]) {
-            return json_encode(array(
+        $recID = 0;
 
-            ));
+        if((boolean)$request->input()["lehrer"]) {
+            $recID = DB::select("SELECT * FROM users WHERE name = :name", array("name" => $request->input()["name"]));
+        } else {
+            $recID = DB::select("SELECT * FROM users WHERE name = :name", array("name" => $request->input()["name"]));
         }
+        
+        if(count($recID) > 0) {
+            $recID = $recID[0]->id;
+        } else {
+            //return redirect("home");
+        }
+        
+        $self = Auth::id();
+
+        $pdo = DB::connection()->getPdo();
+        
+        $stmt = $pdo->prepare("SELECT * FROM chat_messages WHERE (`author` = ? AND `recipient` = ?) OR (`author` = ? AND `recipient` = ?) ORDER BY created_at");
+        $stmt->bindParam(1, $self);
+        $stmt->bindParam(2, $recID);
+        $stmt->bindParam(3, $recID);
+        $stmt->bindParam(4, $self);
+        $stmt->execute();
+        
+        $messages = $stmt->fetchAll();
+        
+        return view("chat", array(
+            "messages" => $messages,
+            "otherName" => $request->input()["name"]
+        ));
     }
 }
