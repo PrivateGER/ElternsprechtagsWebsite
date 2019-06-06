465b75c55186eaecebc6b72161a7ff4254e70c8c
PerhapsSomeone <ma2003br@gmail.com>
2019-06-06 19:26:02 +0200

    Added Sentry.io

--- app/Http/Controllers/PShieldController.php
+++ app/Http/Controllers/PShieldController.php
@@ -8,8 +8,8 @@ class PShieldController extends Controller
 {
     public function banIP() {
         $ban = new \App\IPBans;
-        $ban->ip = $_SERVER["REMOTE_ADDR"];
-        $ban->banHash = hash("sha256", $_SERVER["REMOTE_ADDR"] . getenv("APP_KEY"));
+        $ban->ip = request()->ip();
+        $ban->banHash = hash("sha256", request()->ip() . getenv("APP_KEY"));
         $ban->save();
         return redirect("blockedByPShield");
     }
